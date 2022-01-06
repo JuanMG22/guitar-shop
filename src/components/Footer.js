a62690046245306aeb6320142cfc14c988d5fe2f
Juan Manuel González <juan.m-gonzalez@outlook.com>
2022-01-05 21:18:22 -0300

    update json

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,15 +1,17 @@
+import { Link } from "react-router-dom"
+
 const footer = () => {
     return (
     <footer className="flex justify-center px-4 text-gray-100 bg-gray-800">
         <div className="container py-6">
             <h3 className="text-center text-lg font-bold lg:text-2xl">
-                Join 31,000+ other and never miss <br /> out on new tips, tutorials, and more.
+                Suscribite y no te pierdas nuestros <br /> últimos productos, descuentos y más
             </h3>
 
             <div className="flex justify-center mt-6">
                 <div className="bg-white rounded-lg">
                     <div className="flex flex-wrap justify-between md:flex-row">
-                        <input type="email" className="m-1 p-2 appearance-none text-gray-700 text-sm focus:outline-none" placeholder="Enter your email" />
+                        <input type="email" className="m-1 p-2 appearance-none text-gray-700 text-sm focus:outline-none" placeholder="Ingresa tu email" />
                         <button className="w-full m-1 p-2 text-sm bg-gray-800 rounded-lg font-semibold uppercase lg:w-auto">Enviar</button>
                     </div>
                 </div>
@@ -19,13 +21,13 @@ const footer = () => {
 
             <div className="flex flex-col items-center justify-between mt-6 md:flex-row">
                 <div>
-                    <a href="#" className="text-xl font-bold">Guitar Shop</a>
+                    <Link to="/" className="text-xl font-bold">Guitar Shop</Link>
                 </div>
                 <div className="flex mt-4 md:m-0">
                     <div className="-mx-4">
-                      <a href="#" className="px-4 text-sm">Inicio</a>
-                      <a href="#" className="px-4 text-sm">Productos</a>
-                      <a href="#" className="px-4 text-sm">Acerca de</a>
+                      <Link to="/" className="px-4 text-sm">Inicio</Link>
+                      <Link to="/" className="px-4 text-sm">Productos</Link>
+                      <Link to="/acerca" className="px-4 text-sm">Acerca de</Link>
                   	</div>
                 </div>
             </div>
--- src/components/Item.js
+++ src/components/Item.js
@@ -3,12 +3,12 @@ import { Link } from "react-router-dom";
 const Item = ({ titulo, precio, pictureUrl, id }) => {
   return (
     <>
-      <article className="max-w-xs bg-white border border-gray-900 rounded-lg overflow-hidden my-20 ml-5 hover:shadow-lg hover:shadow-gray-900 hover:scale-105 transition-all">
-        <div className="px-4 py-2">
+      <article className="max-w-xs bg-white border border-gray-900 rounded-lg overflow-hidden my-10 ml-5 hover:shadow-lg hover:shadow-gray-900 hover:scale-105 transition-all">
+        <div className="px-4 py-2 text-center">
           <h1 className="text-gray-900 font-bold text-2xl uppercase">
             {titulo}
           </h1>
-          <p className="text-gray-600 text-sm mt-1">
+          <p className="text-gray-600 text-sm mt-1 text-left">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi quos
             quidem sequi illum facere recusandae voluptatibus
           </p>
