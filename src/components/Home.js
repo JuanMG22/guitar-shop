567be9174d17eafbbd21c906e314a67c0b551e7a
Juan Manuel González <juan.m-gonzalez@outlook.com>
2022-02-06 08:27:25 -0300

    se agregó breadcrumbs

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,13 +5,22 @@ const Home = () => {
     <section className="my-5">
       <div className="container mx-auto px-6">
         <div className="h-64 rounded-md overflow-hidden bg-cover bg-right sm:bg-center bg-[url('https://vertabelo.com/blog/a-data-model-for-an-online-musical-equipment-shop/row-of-different-electric-guitars-hanging-in-a-modern-musical-shop.jpg')]">
-          <div className="bg-gray-900 bg-opacity-70 hover:bg-opacity-50  flex items-center h-full transition-all">
+          <div className="bg-gray-900 bg-opacity-60 sm:bg-opacity-60 hover:bg-opacity-50  flex items-center h-full transition-all">
             <div className="px-10 max-w-xl">
-              <Link to="/productos" className="text-2xl text-white font-semibold">Equipamiento Musical</Link>
+              <Link
+                to="/productos"
+                className="text-2xl text-white font-semibold"
+              >
+                Equipamiento Musical
+              </Link>
               <p className="mt-2 text-gray-100">
-                En Guitar Shop encontrarás el mejor equipamiento musical de las mejores marcas como Musicman, Ibanez, Fender, entre otros.
+                En Guitar Shop encontrarás el mejor equipamiento musical de las
+                mejores marcas como Musicman, Ibanez, Fender, entre otros.
               </p>
-              <Link to="/productos" className="flex items-center mt-4 px-3 py-2  text-white text-sm uppercase font-medium rounded hover:underline focus:outline-none w-1/2 sm:w-1/4 ">
+              <Link
+                to="/productos"
+                className="flex items-center mt-4 px-3 py-2  text-white text-sm uppercase font-medium rounded hover:underline focus:outline-none w-1/2 sm:w-1/4 "
+              >
                 <span>Ver más</span>
                 <svg
                   className="h-5 w-5 mx-2"
@@ -27,13 +36,22 @@ const Home = () => {
         </div>
         <div className="md:flex mt-8 md:-mx-4">
           <div className="w-full h-64 md:mx-4 rounded-md overflow-hidden bg-cover bg-center md: bg-[url('https://www.itl.cat/pngfile/big/150-1507809_john-petrucci-slide-electric-guitar.jpg')]">
-            <div className="bg-gray-900 bg-opacity-70 hover:bg-opacity-50  flex items-center h-full">
+            <div className="bg-gray-900 bg-opacity-60 sm:bg-opacity-60 hover:bg-opacity-50  flex items-center h-full">
               <div className="px-10 max-w-xl">
-                <Link to="/categoria/Guitarras" className="text-2xl text-white font-semibold">Guitarras Electricas</Link>
+                <Link
+                  to="/categoria/Guitarras"
+                  className="text-2xl text-white font-semibold"
+                >
+                  Guitarras Electricas
+                </Link>
                 <p className="mt-2 text-gray-100">
-                  En Guitar Shop encontrarás las mejores guitarras eléctricas de las principales marcas
+                  En Guitar Shop encontrarás las mejores guitarras eléctricas de
+                  las principales marcas
                 </p>
-                <Link to="/categoria/Guitarras" className="flex items-center mt-4 text-white text-sm uppercase font-medium rounded hover:underline focus:outline-none w-1/2 sm:w-1/4 ">
+                <Link
+                  to="/categoria/Guitarras"
+                  className="flex items-center mt-4 text-white text-sm uppercase font-medium rounded hover:underline focus:outline-none w-1/2 sm:w-1/4 "
+                >
                   <span>Ver Más</span>
                   <svg
                     className="h-5 w-5 mx-2"
@@ -48,13 +66,22 @@ const Home = () => {
             </div>
           </div>
           <div className="w-full h-64 mt-8 md:mx-4 rounded-md overflow-hidden bg-cover bg-center md:mt-0 md: bg-[url('https://www.itl.cat/pngfile/big/318-3180622_bass-guitar-hd-wallpapers-jaguar-bass-guitar-kit.jpg')]">
-            <div className="bg-gray-900 bg-opacity-70 hover:bg-opacity-50  flex items-center h-full">
+            <div className="bg-gray-900 bg-opacity-60 sm:bg-opacity-60 hover:bg-opacity-50  flex items-center h-full">
               <div className="px-10 max-w-xl">
-                <Link to="/categoria/Bajos" className="text-2xl text-white font-semibold">Bajos Eléctricos</Link>
+                <Link
+                  to="/categoria/Bajos"
+                  className="text-2xl text-white font-semibold"
+                >
+                  Bajos Eléctricos
+                </Link>
                 <p className="mt-2 text-gray-100">
-                  En Guitar Shop encontrarás los mejores bajos eléctricas de las principales marcas
+                  En Guitar Shop encontrarás los mejores bajos eléctricas de las
+                  principales marcas
                 </p>
-                <Link to="/categoria/Bajos" className="flex items-center mt-4 text-white text-sm uppercase font-medium rounded hover:underline focus:outline-none w-1/2 sm:w-1/4 ">
+                <Link
+                  to="/categoria/Bajos"
+                  className="flex items-center mt-4 text-white text-sm uppercase font-medium rounded hover:underline focus:outline-none w-1/2 sm:w-1/4 "
+                >
                   <span>Ver Más</span>
                   <svg
                     className="h-5 w-5 mx-2"
@@ -71,13 +98,22 @@ const Home = () => {
         </div>
         <div className="md:flex mt-8 md:-mx-4">
           <div className="w-full h-64 md:mx-4 rounded-md overflow-hidden bg-cover bg-center md: bg-[url('https://blog.madridhifi.com/wp-content/uploads/2021/12/mejor-looper-para-guitarra.jpg')]">
-            <div className="bg-gray-900 bg-opacity-70 hover:bg-opacity-50  flex items-center h-full">
+            <div className="bg-gray-900 bg-opacity-60 sm:bg-opacity-60 hover:bg-opacity-50  flex items-center h-full">
               <div className="px-10 max-w-xl">
-                <Link to="/categoria/Pedales" className="text-2xl text-white font-semibold">Pedales</Link>
+                <Link
+                  to="/categoria/Pedales"
+                  className="text-2xl text-white font-semibold"
+                >
+                  Pedales
+                </Link>
                 <p className="mt-2 text-gray-100">
-                  En Guitar Shop encontrarás los mejores pedales de las principales marcas.
+                  En Guitar Shop encontrarás los mejores pedales de las
+                  principales marcas.
                 </p>
-                <Link to="/categoria/Pedales" className="flex items-center mt-4 text-white text-sm uppercase font-medium rounded hover:underline focus:outline-none w-1/2 sm:w-1/4 ">
+                <Link
+                  to="/categoria/Pedales"
+                  className="flex items-center mt-4 text-white text-sm uppercase font-medium rounded hover:underline focus:outline-none w-1/2 sm:w-1/4 "
+                >
                   <>Ver Más</>
                   <svg
                     className="h-5 w-5 mx-2"
@@ -92,13 +128,22 @@ const Home = () => {
             </div>
           </div>
           <div className="w-full h-64 mt-8 md:mx-4 rounded-md overflow-hidden bg-cover bg-center md:mt-0 md: bg-[url('https://wallpapercave.com/wp/wp3784109.jpg')]">
-            <div className="bg-gray-900 bg-opacity-70 hover:bg-opacity-50  flex items-center h-full">
+            <div className="bg-gray-900 bg-opacity-60 sm:bg-opacity-60 hover:bg-opacity-50  flex items-center h-full">
               <div className="px-10 max-w-xl">
-                <Link to="/categoria/Amps" className="text-2xl text-white font-semibold">Amplificadores</Link>
+                <Link
+                  to="/categoria/Amps"
+                  className="text-2xl text-white font-semibold"
+                >
+                  Amplificadores
+                </Link>
                 <p className="mt-2 text-gray-100">
-                  En Guitar Shop encontrarás los mejores amplificadores de las principales marcas
+                  En Guitar Shop encontrarás los mejores amplificadores de las
+                  principales marcas
                 </p>
-                <Link to="/categoria/Amps" className="flex items-center mt-4 text-white text-sm uppercase font-medium rounded hover:underline focus:outline-none w-1/2 sm:w-1/4">
+                <Link
+                  to="/categoria/Amps"
+                  className="flex items-center mt-4 text-white text-sm uppercase font-medium rounded hover:underline focus:outline-none w-1/2 sm:w-1/4"
+                >
                   <span>Ver Más</span>
                   <svg
                     className="h-5 w-5 mx-2"
--- src/components/ItemDetail/ItemDetail.js
+++ src/components/ItemDetail/ItemDetail.js
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import { SRLWrapper } from "simple-react-lightbox";
 
 const ItemDetail = ({ producto, isAdded }) => {
@@ -7,10 +8,52 @@ const ItemDetail = ({ producto, isAdded }) => {
         Detalle de producto
       </h2>
       <div className="container p-5 mx-auto">
+        <div className="flex justify-center">
+          <ol className="list-none p-0 inline-flex">
+            <li className="flex items-center">
+              <Link to="/">Inicio</Link>
+              <svg
+                className="fill-current w-3 h-3 mx-2"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 320 512"
+              >
+                <path d="M285.476 272.971L91.132 467.314c-9.373 9.373-24.569 9.373-33.941 0l-22.667-22.667c-9.357-9.357-9.375-24.522-.04-33.901L188.505 256 34.484 101.255c-9.335-9.379-9.317-24.544.04-33.901l22.667-22.667c9.373-9.373 24.569-9.373 33.941 0L285.475 239.03c9.373 9.372 9.373 24.568.001 33.941z" />
+              </svg>
+            </li>
+            <li className="flex items-center">
+              <Link to={`/productos`}>Productos</Link>
+              <svg
+                className="fill-current w-3 h-3 mx-2"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 320 512"
+              >
+                <path d="M285.476 272.971L91.132 467.314c-9.373 9.373-24.569 9.373-33.941 0l-22.667-22.667c-9.357-9.357-9.375-24.522-.04-33.901L188.505 256 34.484 101.255c-9.335-9.379-9.317-24.544.04-33.901l22.667-22.667c9.373-9.373 24.569-9.373 33.941 0L285.475 239.03c9.373 9.372 9.373 24.568.001 33.941z" />
+              </svg>
+            </li>
+            <li className="flex items-center">
+              <Link to={`/categoria/${producto.categoria}`}>
+                {producto.categoria}
+              </Link>
+              <svg
+                className="fill-current w-3 h-3 mx-2"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 320 512"
+              >
+                <path d="M285.476 272.971L91.132 467.314c-9.373 9.373-24.569 9.373-33.941 0l-22.667-22.667c-9.357-9.357-9.375-24.522-.04-33.901L188.505 256 34.484 101.255c-9.335-9.379-9.317-24.544.04-33.901l22.667-22.667c9.373-9.373 24.569-9.373 33.941 0L285.475 239.03c9.373 9.372 9.373 24.568.001 33.941z" />
+              </svg>
+            </li>
+            <li>
+              <p className="text-gray-500" aria-current="page">
+                {producto.titulo}
+              </p>
+            </li>
+          </ol>
+        </div>
         <div className="lg:w-4/5 mx-auto flex flex-wrap">
           <div className="lg:w-1/2 w-full h-1/2 mt-6 hover:opacity-80 object-contain object-center border border-gray-200 cursor-pointer transition-all">
             <SRLWrapper>
               <img alt={producto.titulo} src={producto.imagen} />
+              <img alt={producto.titulo} src={producto.imagen2} className="hidden"/>
             </SRLWrapper>
           </div>
           <div className="lg:w-1/2 w-full lg:pl-10 lg:py-6 mt-6 lg:mt-0">
@@ -20,12 +63,10 @@ const ItemDetail = ({ producto, isAdded }) => {
             <h1 className="text-gray-900 text-3xl title-font font-medium mb-1">
               {producto.titulo}
             </h1>
-            <p className="leading-relaxed">
-              {producto.descripcion}
-            </p>
+            <p className="leading-relaxed">{producto.descripcion}</p>
             <div className="flex lg:flex-nowrap sm:flex-wrap my-5">
               <span className="title-font font-medium text-2xl text-gray-900">
-                $ {producto.precio.toLocaleString('es-AR')}
+                $ {producto.precio.toLocaleString("es-AR")}
               </span>
               {isAdded}
             </div>
